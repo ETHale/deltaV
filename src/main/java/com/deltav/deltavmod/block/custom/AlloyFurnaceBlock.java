d11392ff8592daceb7f866c95f619a31b572a4d7
Jack <118830120+Derringo@users.noreply.github.com>
2025-06-27 23:29:09 +0100

    Added non-functional GUI & partical effects for alloy furnace

--- src/main/java/com/deltav/deltavmod/block/custom/AlloyFurnaceBlock.java
+++ src/main/java/com/deltav/deltavmod/block/custom/AlloyFurnaceBlock.java
@@ -4,25 +4,36 @@ package com.deltav.deltavmod.block.custom;
 import javax.annotation.Nullable;
 
 import com.deltav.deltavmod.block.entity.AlloyFurnaceBlockEntity;
+import com.mojang.serialization.MapCodec;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 
-public class AlloyFurnaceBlock extends Block implements EntityBlock{
 
-    public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
-    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+public class AlloyFurnaceBlock extends AbstractFurnaceBlock {
+
+    public static final MapCodec<AlloyFurnaceBlock> CODEC = simpleCodec(AlloyFurnaceBlock::new);
+
+    @Override
+    public MapCodec<AlloyFurnaceBlock> codec() {
+        return CODEC;
+    }
 
     public AlloyFurnaceBlock(Properties properties) {
         super(properties);
@@ -52,5 +63,40 @@ public class AlloyFurnaceBlock extends Block implements EntityBlock{
         return new AlloyFurnaceBlockEntity(pos, state);
     }
 
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return createFurnaceTicker(level, type, BlockEntityType.FURNACE);
+    }
+
+    @Override
+    protected void openContainer(Level level, BlockPos pos, Player player) {
+        BlockEntity blockentity = level.getBlockEntity(pos);
+        if (blockentity instanceof AlloyFurnaceBlockEntity) {
+            player.openMenu((MenuProvider)blockentity);
+        }
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        if (state.getValue(LIT)) {
+            double d0 = pos.getX() + 0.5;
+            double d1 = pos.getY();
+            double d2 = pos.getZ() + 0.5;
+            if (random.nextDouble() < 0.1) {
+                level.playLocalSound(d0, d1, d2, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            Direction direction = state.getValue(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d3 = 0.52;
+            double d4 = random.nextDouble() * 0.6 - 0.3;
+            double d5 = direction$axis == Direction.Axis.X ? direction.getStepX() * 0.52 : d4;
+            double d6 = random.nextDouble() * 6.0 / 16.0;
+            double d7 = direction$axis == Direction.Axis.Z ? direction.getStepZ() * 0.52 : d4;
+            level.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0, 0.0, 0.0);
+            level.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0, 0.0, 0.0);
+        }
+    }
 
 }
--- src/main/java/com/deltav/deltavmod/block/entity/AlloyFurnaceBlockEntity.java
+++ src/main/java/com/deltav/deltavmod/block/entity/AlloyFurnaceBlockEntity.java
@@ -1,24 +1,37 @@
 package com.deltav.deltavmod.block.entity;
 
+
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.BlastFurnaceMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.world.level.block.entity.FuelValues;
 import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.neoforge.items.ItemStackHandler;
 
-public class AlloyFurnaceBlockEntity extends BlockEntity{
+public class AlloyFurnaceBlockEntity extends AbstractFurnaceBlockEntity {
 
-    public final ItemStackHandler inventory = new ItemStackHandler(1) {
-        @Override
-        protected void onContentsChanged(int slot) {
-            setChanged();
-            if(!level.isClientSide()) {
-                level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), 3);
-            }
-        }
-    };
-
-    public AlloyFurnaceBlockEntity(BlockPos pos, BlockState blockState) {
-        super(ModBlockEntities.ALLOY_FURNACE_BE.get(), pos, blockState);
+public AlloyFurnaceBlockEntity(BlockPos pos, BlockState blockState) {
+        super(ModBlockEntities.ALLOY_FURNACE_BE.get(), pos, blockState, RecipeType.BLASTING);
     }
 
+    @Override
+    protected Component getDefaultName() {
+        return Component.translatable("container.alloy_furnace");
+    }
+
+    @Override
+    protected int getBurnDuration(FuelValues fuelValues, ItemStack stack) {
+        return super.getBurnDuration(fuelValues, stack) / 2;
+    }
+
+    @Override
+    protected AbstractContainerMenu createMenu(int id, Inventory player) {
+        return new BlastFurnaceMenu(id, player, this, this.dataAccess);
+    }
+
+    
 }
