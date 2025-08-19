99bb716f6ec99a5391341b0b5f76af202164d045
adampy <adamflint8@hotmail.co.uk>
2025-08-19 14:47:10 +0100

    Removed unused imports from changed files

--- src/main/java/com/deltav/deltavmod/DeltaVClient.java
+++ src/main/java/com/deltav/deltavmod/DeltaVClient.java
@@ -1,22 +1,10 @@
 package com.deltav.deltavmod;
 
-import com.deltav.deltavmod.fluid.ModFluidTypes;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BiomeColors;
-import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.material.FluidState;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
-import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.common.NeoForgeMod;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = DeltaV.MODID, dist = Dist.CLIENT)
