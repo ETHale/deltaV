d37e7a8fb84f84af14a71f184945dd6f51da2ca6
Ethan <bloxinggamesfy@gmail.com>
2026-01-11 12:52:56 +0000

    feat: improved config and fixed surface and edge placement

--- src/main/java/com/deltav/deltavmod/worldgen/features/HotSpringFeature.java
+++ src/main/java/com/deltav/deltavmod/worldgen/features/HotSpringFeature.java
@@ -9,6 +9,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
@@ -33,33 +34,43 @@ public class HotSpringFeature extends Feature<HotSpringFeatureConfiguration> {
         BlockPos origin = context.origin();
 
         // Create a sunken ground area
-        int radius = conf.radius();
-        int depth =  conf.depth();
+        int radius = conf.radius().sample(randomsource);
+        int depth =  conf.depth().sample(randomsource);
         Set<BlockPos>[] areas = createSunkenGround(worldgenlevel, origin, radius, depth, conf, randomsource);
         Set<BlockPos> poolArea = areas[0];
         Set<BlockPos> edgeArea = areas[1];
+        Set<BlockPos> surfaceArea = areas[2];
 
         // Waterlog the area
         waterlogArea(worldgenlevel, poolArea, conf);
 
+        // Place edge blocks
+        placeEdgeBlocks(worldgenlevel, edgeArea, conf, randomsource);
+
+        // Place surface blocks
+        placeSurfaceBlocks(worldgenlevel, surfaceArea, conf, randomsource);
+
         // Place a geyser
-        placeGeyser(worldgenlevel, edgeArea, conf);
+        placeGeyser(worldgenlevel, surfaceArea, conf);
 
         return true;
     }
 
     private Set<BlockPos>[] createSunkenGround(WorldGenLevel level, BlockPos origin, int radius, int depth, HotSpringFeatureConfiguration conf, RandomSource rand) {
         Set<BlockPos> poolArea = new HashSet<>();
+        Set<BlockPos> notWaterLoggedPoolArea = new HashSet<>();
         Set<BlockPos> edgeArea = new HashSet<>();
+        Set<BlockPos> surfaceArea = new HashSet<>();
         BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
 
-        int edgeWidth = conf.edgeWidth();
+        int edgeWidth = conf.edgeWidth().sample(rand);
+        int contentsDepth = conf.contentsDepth().sample(rand);
         for (int y = 0; y < depth; y++) {
             // Adjust noise to create jagged edges
-            int newRadius = (int)(radius - (conf.rateOfChange() * y));
+            int newRadius = (int)(radius - (conf.rateOfChange().sample(rand) * y));
             
             //move centre around to add more noise 
-            int originNoise = conf.originNoise();
+            int originNoise = conf.originNoise().sample(rand);
             BlockPos noisyOrigin = origin.offset(rand.nextInt(originNoise) - (originNoise / 2), 0, rand.nextInt(originNoise) - (originNoise / 2));
             
             int areaSize = (int) newRadius + (int) edgeWidth;
@@ -70,12 +81,16 @@ public class HotSpringFeature extends Feature<HotSpringFeatureConfiguration> {
                 float point = dx * dx + dz * dz;
                 
                 // Check if the block is within the pool area
-                float noise = rand.nextFloat() * conf.noiseScale(); 
+                float noise = rand.nextFloat() * conf.noiseScale().sample(rand); 
                 float threshold = (1.0f - noise) * newRadius * newRadius;
                 if (point <= threshold) {
                     mutablePos.set(pos.getX(), noisyOrigin.getY() - y, pos.getZ());
-                    if (canReplaceBlock(level.getBlockState(mutablePos))) {
+                    if (canReplaceBlock(level.getBlockState(mutablePos), conf)) {
                         poolArea.add(mutablePos.immutable());
+                        if (y < contentsDepth) {
+                            notWaterLoggedPoolArea.add(mutablePos.immutable());
+                            level.setBlock(mutablePos, Blocks.AIR.defaultBlockState(), 2);
+                        }
                     }
                 }
             }
@@ -85,21 +100,30 @@ public class HotSpringFeature extends Feature<HotSpringFeatureConfiguration> {
         for (BlockPos pos : poolArea) {
             for (int dx = -edgeWidth; dx <= edgeWidth; dx++) {
                 for (int dz = -edgeWidth; dz <= edgeWidth; dz++) {
-                    for (int dy = 0; dy > -2; dy--) {
+                    for (int dy = 0; dy > -edgeWidth; dy--) {
                         mutablePos.set(pos.offset(dx, dy, dz));
-                        if (!poolArea.contains(mutablePos) && canReplaceBlock(level.getBlockState(mutablePos)) 
-                            && !edgeArea.contains(mutablePos) && level.getFluidState(mutablePos) != conf.contents()) {
-                            level.setBlock(mutablePos, conf.barrier().getState(rand, mutablePos), 2);
-                            this.markAboveForPostProcessing(level, pos);
-                            edgeArea.add(mutablePos);
+                        if (!poolArea.contains(mutablePos) && canReplaceBlock(level.getBlockState(mutablePos), conf)
+                            && !edgeArea.contains(mutablePos.immutable()) && level.getFluidState(mutablePos) != conf.contents()) {
+                            edgeArea.add(mutablePos.immutable());
                         }
                     }
                 }
             }
         }
+        poolArea.removeAll(edgeArea);
 
+        // loop through edgeArea to find those that touch the surface
+        for (BlockPos pos : edgeArea) {
+            BlockPos abovePos = pos.above();
+            if (((poolArea.contains(abovePos) || level.getBlockState(abovePos).isAir()) && !edgeArea.contains(abovePos))) {
+                surfaceArea.add(new BlockPos(pos));
+            }
+        }
+        edgeArea.removeAll(surfaceArea);
+        poolArea.removeAll(notWaterLoggedPoolArea);
+        
         @SuppressWarnings("unchecked")
-        Set<BlockPos>[] result = (Set<BlockPos>[]) new Set[]{poolArea, edgeArea};
+        Set<BlockPos>[] result = (Set<BlockPos>[]) new Set[]{poolArea, edgeArea, surfaceArea};
         return result;
     }
 
@@ -109,27 +133,31 @@ public class HotSpringFeature extends Feature<HotSpringFeatureConfiguration> {
             this.markAboveForPostProcessing(level, pos);
         }
     }
+    
+    private void placeEdgeBlocks(WorldGenLevel level, Set<BlockPos> edgeArea, HotSpringFeatureConfiguration conf, RandomSource rand) {
+        for (BlockPos pos : edgeArea) {
+            level.setBlock(pos, conf.barrier().getState(rand, pos), 2);
+            this.markAboveForPostProcessing(level, pos);
+        }
+    }
+    
+    private void placeSurfaceBlocks(WorldGenLevel level, Set<BlockPos> surfaceArea, HotSpringFeatureConfiguration conf, RandomSource rand) {
+        for (BlockPos pos : surfaceArea) {
+            level.setBlock(pos, conf.surface().getState(rand, pos), 2);
+            this.markAboveForPostProcessing(level, pos);
+        }
+    }
 
     private void placeGeyser(WorldGenLevel level, Set<BlockPos> area, HotSpringFeatureConfiguration conf) {
         if (area.isEmpty()) return;
 
-        // Filter positions to include only those visible to the sky
-        Set<BlockPos> skyVisiblePositions = new HashSet<>();
-        for (BlockPos pos : area) {
-            if (level.canSeeSky(pos)) {
-                skyVisiblePositions.add(pos);
-            }
-        }
-
-        if (skyVisiblePositions.isEmpty()) return;
-
         // Select a random position from the filtered set
-        BlockPos[] positions = skyVisiblePositions.toArray(new BlockPos[0]);
+        BlockPos[] positions = area.toArray(new BlockPos[0]);
         BlockPos geyserPos = positions[level.getRandom().nextInt(positions.length)];
         level.setBlock(geyserPos, conf.geyser(), 2);
     }
 
-    private boolean canReplaceBlock(BlockState state) {
-        return !state.is(BlockTags.FEATURES_CANNOT_REPLACE);
+    private boolean canReplaceBlock(BlockState state, HotSpringFeatureConfiguration conf) {
+        return !state.is(BlockTags.FEATURES_CANNOT_REPLACE) && state != conf.geyser();
     }
 }
--- src/main/java/com/deltav/deltavmod/worldgen/features/HotSpringFeatureConfiguration.java
+++ src/main/java/com/deltav/deltavmod/worldgen/features/HotSpringFeatureConfiguration.java
@@ -2,7 +2,6 @@ package com.deltav.deltavmod.worldgen.features;
 
 import com.deltav.deltavmod.block.ModBlocks;
 import com.deltav.deltavmod.fluid.ModFluids;
-import com.mojang.datafixers.types.templates.List;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.block.state.BlockState;
@@ -11,44 +10,55 @@ import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvi
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.util.random.WeightedList;
+import net.minecraft.util.valueproviders.FloatProvider;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.util.valueproviders.ConstantFloat;
+import net.minecraft.util.valueproviders.ConstantInt;
 
 public record HotSpringFeatureConfiguration(
     FluidState contents,
     BlockStateProvider barrier,     
+    BlockStateProvider surface,     
     BlockState geyser,
-    int depth,
-    int radius,
-    int edgeWidth,
-    float noiseScale,
-    float rateOfChange,
-    int originNoise
+    IntProvider depth,
+    IntProvider contentsDepth,
+    IntProvider radius,
+    IntProvider edgeWidth,
+    FloatProvider noiseScale,
+    FloatProvider rateOfChange,
+    IntProvider originNoise
 ) implements FeatureConfiguration {
     public static final Codec<HotSpringFeatureConfiguration> CODEC = RecordCodecBuilder.create(inst -> inst.group(
         FluidState.CODEC.fieldOf("contents").orElse(ModFluids.THERMAL_WATER_SOURCE.get().defaultFluidState()).forGetter(HotSpringFeatureConfiguration::contents),
         BlockStateProvider.CODEC.fieldOf("barrier").orElse(BlockStateProvider.simple(ModBlocks.SILICA_SANDSTONE.get().defaultBlockState())).forGetter(HotSpringFeatureConfiguration::barrier),
+        BlockStateProvider.CODEC.fieldOf("surface").orElse(BlockStateProvider.simple(ModBlocks.SILICA_SANDSTONE.get().defaultBlockState())).forGetter(HotSpringFeatureConfiguration::surface),
         BlockState.CODEC.fieldOf("geyser").orElse(Blocks.AIR.defaultBlockState()).forGetter(HotSpringFeatureConfiguration::geyser),
-        Codec.intRange(0, 255).fieldOf("depth").orElse(6).forGetter(HotSpringFeatureConfiguration::depth),
-        Codec.intRange(0, 255).fieldOf("radius").orElse(8).forGetter(HotSpringFeatureConfiguration::radius),
-        Codec.intRange(0, 255).fieldOf("edgeWidth").orElse(2).forGetter(HotSpringFeatureConfiguration::edgeWidth),
-        Codec.floatRange(0, 1).fieldOf("noiseScale").orElse(0.4f).forGetter(HotSpringFeatureConfiguration::noiseScale),
-        Codec.floatRange(0, 255).fieldOf("rateOfChange").orElse(1.3f).forGetter(HotSpringFeatureConfiguration::rateOfChange),
-        Codec.intRange(0, 255).fieldOf("originNoise").orElse(3).forGetter(HotSpringFeatureConfiguration::originNoise)
+        IntProvider.CODEC.fieldOf("depth").orElse(ConstantInt.of(6)).forGetter(HotSpringFeatureConfiguration::depth),
+        IntProvider.CODEC.fieldOf("contentsDepth").orElse(ConstantInt.of(0)).forGetter(HotSpringFeatureConfiguration::contentsDepth),
+        IntProvider.CODEC.fieldOf("radius").orElse(ConstantInt.of(8)).forGetter(HotSpringFeatureConfiguration::radius),
+        IntProvider.CODEC.fieldOf("edgeWidth").orElse(ConstantInt.of(2)).forGetter(HotSpringFeatureConfiguration::edgeWidth),
+        FloatProvider.CODEC.fieldOf("noiseScale").orElse(ConstantFloat.of(0.4f)).forGetter(HotSpringFeatureConfiguration::noiseScale),
+        FloatProvider.CODEC.fieldOf("rateOfChange").orElse(ConstantFloat.of(1.3f)).forGetter(HotSpringFeatureConfiguration::rateOfChange),
+        IntProvider.CODEC.fieldOf("originNoise").orElse(ConstantInt.of(3)).forGetter(HotSpringFeatureConfiguration::originNoise)
     ).apply(inst, HotSpringFeatureConfiguration::new));
 
     public static final HotSpringFeatureConfiguration INSTANCE = new HotSpringFeatureConfiguration(
         ModFluids.THERMAL_WATER_SOURCE.get().defaultFluidState(),
+        BlockStateProvider.simple(ModBlocks.SILICA_SANDSTONE.get().defaultBlockState()),
         new WeightedStateProvider(
             WeightedList.<BlockState>builder()
-                .add(ModBlocks.SILICA_SANDSTONE.get().defaultBlockState(), 5)
-                .add(ModBlocks.SILICA_SAND.get().defaultBlockState(), 1).build()
+                .add(ModBlocks.SILICA_SANDSTONE.get().defaultBlockState(), 2)
+                .add(ModBlocks.SILICA_SAND.get().defaultBlockState(), 3).build()
         ),
         ModBlocks.STEAM_GEYSER.get().defaultBlockState(),
-        6,
-        8,
-        2,
-        0.4f,
-        1.3f,
-        3
+        UniformInt.of(5,9),
+        UniformInt.of(0,2),
+        UniformInt.of(6,10),
+        ConstantInt.of(2),
+        ConstantFloat.of(0.4f),
+        ConstantFloat.of(1.3f),
+        ConstantInt.of(3)
     );
 }
