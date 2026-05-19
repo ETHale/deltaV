1caaafaab2208b196eb300397d81afdceecbad10
Ethan <bloxinggamesfy@gmail.com>
2026-05-19 18:13:06 +0100

    feat: added custom shape for tree tapper

--- src/main/java/com/deltav/deltavmod/block/custom/TreeTapperBlock.java
+++ src/main/java/com/deltav/deltavmod/block/custom/TreeTapperBlock.java
@@ -1,7 +1,9 @@
 package com.deltav.deltavmod.block.custom;
 import com.mojang.serialization.MapCodec;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
@@ -9,11 +11,16 @@ import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TreeTapperBlock extends HorizontalDirectionalBlock{
     public static final MapCodec<TreeTapperBlock> CODEC = simpleCodec(TreeTapperBlock::new);
-
     public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
+    private static final VoxelShape SHAPE_NORTH = Block.box(6.0, 2.0, 14.0, 10.0, 6.0, 16.0);
+    private static final VoxelShape SHAPE_SOUTH = Block.box(6.0, 2.0, 0.0, 10.0, 6.0, 2.0);
+    private static final VoxelShape SHAPE_EAST = Block.box(0.0, 2.0, 6.0, 2.0, 6.0, 10.0);
+    private static final VoxelShape SHAPE_WEST = Block.box(14.0, 2.0, 6.0, 16.0, 6.0, 10.0);
     
     public TreeTapperBlock(Properties properties) {
         super(properties);
@@ -39,5 +46,15 @@ public class TreeTapperBlock extends HorizontalDirectionalBlock{
     @Override
     protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return switch(state.getValue(FACING)) {
+            case SOUTH -> SHAPE_SOUTH;
+            case EAST -> SHAPE_EAST;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
     }
 }
